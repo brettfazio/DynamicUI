278d3c6322b73ec785dbfa6efa8ab17661e3d877
Brett Fazio <brett.fazio@hotmail.com>
2021-01-26 14:22:03 -0500

    basic outline

--- Sources/DynamicUI/DynamicUI.swift
+++ Sources/DynamicUI/DynamicUI.swift
@@ -1,3 +1,28 @@
-struct DynamicUI {
-    var text = "Hello, World!"
+import SwiftUI
+
+public struct DynamicUI {
+    
+    public static func get(reference: [String : Any]) -> some View {
+        
+        if reference.isEmpty {
+            
+            var ret: some View  {
+                Text("Dyn")
+            }
+            
+            return AnyView(ret)
+        }else {
+            var ret: some View  {
+                VStack {
+                    Text("Dyn")
+                    Text("SwiftUI")
+                }
+            }
+
+            return AnyView(ret)
+        }
+        
+
+    }
+    
 }
